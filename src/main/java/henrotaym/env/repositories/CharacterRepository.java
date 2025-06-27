c5bc8c06af023dfe9b43922de188aa7e7c05b7ff
Jess-the-pig <135271280+Jess-the-pig@users.noreply.github.com>
2025-06-27 13:03:56 +0200

    WIP : Deleting

--- src/main/java/henrotaym/env/repositories/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package henrotaym.env.repositories;
-
-import henrotaym.env.entities.Character;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface CharacterRepository extends JpaRepository<Character, BigInteger> {}
--- src/main/java/henrotaym/env/repositories/EpisodeRepository.java
+++
@@ -1,9 +0,0 @@
-package henrotaym.env.repositories;
-
-import henrotaym.env.entities.Episode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface EpisodeRepository extends JpaRepository<Episode, BigInteger> {}
